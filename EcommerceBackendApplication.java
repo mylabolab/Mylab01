1805e492d4e7130a602fcbd51eab75eaabfe65ea
mylabolab <zoangamoussa45@gmail.com>
2025-12-08 18:54:24 +0000

    Update EcommerceBackendApplication.java

--- EcommerceBackendApplication.java
+++ EcommerceBackendApplication.java
@@ -9,7 +9,7 @@ public class EcommerceBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceBackendApplication.class, args);
 		int j = 10;
-		
+		sting name = "moses"
 	}
 
 }
