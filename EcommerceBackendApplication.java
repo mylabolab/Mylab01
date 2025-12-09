279082a0e72bc537f0de7e827e26104f0aa61ac3
mosesbreme <assmoudebreme@gmail.com>
2025-12-09 18:35:58 +0000

    JIRA-102 completed

--- EcommerceBackendApplication.java
+++ EcommerceBackendApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcommerceBackendApplication {
 
+// JIRA-102   
+     public void m2() {
+   
+   //Logic
+}
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceBackendApplication.class, args);
 		int i = 10;
